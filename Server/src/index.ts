ab1f985896d125f5e4b0bf3a7c6b7a7c86b0cfd9
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2023-01-27 20:11:47 +0530

    mongoose setup

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { Request,Response} from "express";
+import mongoose from 'mongoose'
 // import dbo from "./db/conn";
 import cors from "cors";
 const app = express();
@@ -7,12 +8,17 @@ require("dotenv").config({ path: "./config.env" });
 const port = process.env.PORT || 5000;
 
 
-app.get('/', (req,res) => {
+app.get('/hello', (req: Request,res: Response) => {
     res.send("Hello world")
 })
 
+app.get('/', (req: Request,res: Response) => {
+    res.send("gg")
+})
+
+
 app.listen(port);
-console.log(`listening to port ` + port)
+console.log(`listening to port ${port}`)
 // app.use(cors());
 // app.use(express.json());
 // app.use(require("./routes/record"));
